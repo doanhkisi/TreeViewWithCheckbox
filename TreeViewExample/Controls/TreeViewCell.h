bc4d8d0a62470f99a2727a9a6159aff0c50ce5f5
Bui Duy Doanh <doanhkisi@doanhkisi.local>
2015-10-27 15:33:38 +0700

    fist commit

---
+++ TreeViewExample/Controls/TreeViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TreeViewCell.h
+//  TreeViewExample
+//
+//  Created by Bui Duy Doanh on 10/27/15.
+//  Copyright © 2015 doanhkisi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TreeViewCell : UITableViewCell
+
+@end
---
+++ TreeViewExample/Controls/TreeViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TreeViewCell.m
+//  TreeViewExample
+//
+//  Created by Bui Duy Doanh on 10/27/15.
+//  Copyright © 2015 doanhkisi. All rights reserved.
+//
+
+#import "TreeViewCell.h"
+
+@implementation TreeViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
